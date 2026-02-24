094517555d2580a5878ef9b911adcc416e8b3d32
planc90 <ceyhun.yilmaz@hotmail.com.tr>
2026-02-24 14:10:59 +0300

    Bumping theme version to 1.4.3 for stable release

--- functions.php
+++ functions.php
@@ -16,7 +16,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Define Theme Version: 1.3.2 for Cache Management & Portability
-define('XEPMARKET_ALFA_VERSION', '1.4.2'); // Updated for Plugin v1.8.8 release
+define('XEPMARKET_ALFA_VERSION', '1.4.3'); // Post-sync stable release
 /**
  * COMPATIBILITY: Prevent Fatal Error if mail() is disabled on server
  * This prevents the site from crashing when WooCommerce or other plugins try to send emails
